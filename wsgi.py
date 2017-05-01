5460f76e7d6399561cdb771471981509c044e135
jj1of6 <jj1of62003@gmail.com>
2017-05-01 17:00:26 -0500

    Update wsgi.py
    For Testing

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! and Jon"
 
 if __name__ == "__main__":
     application.run()
